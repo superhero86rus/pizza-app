a3d8341a06d5b8d383b6c1a260e013bd46c232f0
Kotov Aleksandr <kotov.alexandr@gmail.com>
2025-01-18 22:37:18 +0300

    Outlet. Передача лейаута внутрь

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,27 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-
 import { Menu } from './pages/Menu/Menu';
 import { Cart } from './pages/Cart/Cart';
 import { Error } from './pages/Error/Error';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Layout } from './layout/Menu/Menu';
 
 const router = createBrowserRouter(
 	[
 		{
 			path: '/',
-			element: <Menu />
-		},
-		{
-			path: '/cart',
-			element: <Cart />
+			element: <Layout />,
+			children: [
+				{
+					path: '/',
+					element: <Menu />
+				},
+				{
+					path: '/cart',
+					element: <Cart />
+				}
+			]
 		},
 		{
 			path: '*',
