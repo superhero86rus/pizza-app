d9cdefd1628bb7590370f071a3100a2a9905e759
Kotov Aleksandr <kotov.alexandr@gmail.com>
2025-01-18 22:21:20 +0300

    createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
-import { Route, Routes } from 'react-router-dom';
 import { Menu } from './pages/Menu/Menu';
 import { Cart } from './pages/Cart/Cart';
 import { Error } from './pages/Error/Error';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Menu />
+		},
+		{
+			path: '/cart',
+			element: <Cart />
+		},
+		{
+			path: '*',
+			element: <Error />
+		}
+	], 
+	{
+		future: {
+			v7_relativeSplatPath: true, // Enables relative paths in nested routes
+			v7_fetcherPersist: true,   // Retains fetcher state during navigation
+			v7_normalizeFormMethod: true, // Normalizes form methods (e.g., POST or GET)
+			v7_partialHydration: true, // Supports partial hydration for server-side rendering
+			v7_skipActionErrorRevalidation: true // Prevents revalidation when action errors occur
+		}
+	}
+);
 
 function App() {
 
 	const [counter, setCounter] = useState<number>(0);
 
-	const AddCounter = () => {
-		setCounter(1);
+	const AddCounter = (e: MouseEvent) => {
+		console.log(e);
 	};
   
 	return (
@@ -25,11 +51,7 @@ function App() {
 				<a href="/cart">Корзина</a>
 			</div>
 
-			<Routes>
-				<Route path='/' element={ <Menu /> } />
-				<Route path='/cart' element={ <Cart />} />
-				<Route path='*' element={ <Error />} />
-			</Routes>
+			<RouterProvider router={router} future={{ v7_startTransition: true }} />
 		</>
 	);
 }
