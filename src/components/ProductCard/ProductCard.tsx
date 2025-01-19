0582a77e0747c0fa4d5ff87fac3770bbea7373f1
Kotov Aleksandr <kotov.alexandr@gmail.com>
2025-01-19 17:43:52 +0300

    Получекние данных. fetch

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -22,7 +22,7 @@ function ProductCard(props: ProductCardProps) {
 					</div>
 				</div>
 				<div className={styles['footer']}>
-					<div className={styles['title']}>{props.title}</div>
+					<div className={styles['title']}>{props.name}</div>
 					<div className={styles['description']}>{props.description}</div>
 				</div>
 			</div>
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,23 +1,53 @@
+import { useEffect, useState } from 'react';
 import Headling from '../../components/Headling/Headling';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
+import { PREFIX } from '../../helpers/API';
+import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
 
 export function Menu(){
+
+	const [products, setProducts] = useState<Product[]>([]);
+
+	const getMenu = async () => {
+
+		try{
+			const res = await fetch(`${PREFIX}/products`);
+			if(!res.ok){
+				return;
+			}
+
+			const data = await res.json() as Product[];
+			setProducts(data);
+		}catch(e){
+			console.log(e);
+			return;
+		}
+	};
+
+	useEffect(() => {
+		getMenu();
+	}, []);
+
 	return <>
 		<div className={styles['head']}>
 			<Headling>Меню</Headling>
 		    <Search placeholder="Введите блюдо или состав"/>
 		</div>
 		<div>
-			<ProductCard
-				id={1}
-				title='Наслаждение'
-				description='Салями, рукола, помидоры, оливки'
-				rating={4.5}
-				price={300}
-				image='../../../public/pizza-test.jpg'
-			/>
+			{products.map(p => (
+				<ProductCard
+					key={p.id}
+					id={p.id}
+					name={p.name}
+					description={p.ingredients.join(', ')}
+					rating={p.rating}
+					price={p.price}
+					image={p.image}
+				/>
+			))}
+			
 		</div>
 		
 	</>;
