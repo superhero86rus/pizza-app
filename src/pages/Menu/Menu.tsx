5a7f2cc019245139a12ea73ba138580d30aca966
Kotov Aleksandr <kotov.alexandr@gmail.com>
2025-01-19 13:17:37 +0300

    Стилизация карточки товара

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Headling from '../../components/Headling/Headling';
+import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
 import styles from './Menu.module.css';
 
@@ -7,6 +8,16 @@ export function Menu(){
 		<div className={styles['head']}>
 			<Headling>Меню</Headling>
 		    <Search placeholder="Введите блюдо или состав"/>
+		</div>
+		<div>
+			<ProductCard
+				id={1}
+				title='Наслаждение'
+				description='Салями, рукола, помидоры, оливки'
+				rating={4.5}
+				price={300}
+				image='../../../public/pizza-test.jpg'
+			/>
 		</div>
 		
 	</>;
