023cfbeaaf39c26f7346e37e6d4b3b79652777be
Kotov Aleksandr <kotov.alexandr@gmail.com>
2025-01-26 22:07:57 +0300

    Редактирование корзины

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,25 +1,26 @@
 import styles from './CartItem.module.css';
 import { CartItemProps } from './CartItem.props';
-import LocalMallRoundedIcon from '@mui/icons-material/LocalMallRounded';
-import { MouseEvent } from 'react';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../store/store';
 import { cartActions } from '../../store/cart.slice';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import ClearIcon from '@mui/icons-material/Clear';
 
 function CartItem(props: CartItemProps) {
 
 	const dispatch = useDispatch<AppDispatch>();
 
-	const increase = (/*e: MouseEvent*/) => {
+	const increase = () => {
 		dispatch(cartActions.add(props.id));
 	};
 
-	const decrease = (/*e: MouseEvent*/) => {
-		// dispatch(cartActions.remove(props.id));
+	const decrease = () => {
+		dispatch(cartActions.remove(props.id));
 	};
 
-	const remove = (/*e: MouseEvent*/) => {
-		// dispatch(cartActions.remove(props.id));
+	const remove = () => {
+		dispatch(cartActions.delete(props.id));
 	};
 
 	
@@ -29,19 +30,19 @@ function CartItem(props: CartItemProps) {
 			<div className={styles['image']} style={{backgroundImage: `url('${props.image}')`}}></div>
 			<div className={styles['description']}>
 				<div className={styles['name']}>{props.name}</div>
-				<div className={styles['currency']}>{props.price}&nbsp;₽</div>
+				<div className={styles['price']}>{props.price}&nbsp;₽</div>
 			</div>
 
 			<div className={styles['actions']}>
-				<button className={styles['button']} onClick={decrease}>
-				    <LocalMallRoundedIcon style={{color: 'white'}}/>
+				<button className={styles['minus']} onClick={decrease}>
+				    <RemoveIcon sx={{color: '#FE724C'}} />
 			    </button>
-				<div>{props.count}</div>
-				<button className={styles['button']} onClick={increase}>
-					<LocalMallRoundedIcon style={{color: 'white'}}/>
+				<div className={styles['number']}>{props.count}</div>
+				<button className={styles['plus']} onClick={increase}>
+					<AddIcon style={{color: 'white'}}/>
 				</button>
-				<button className={styles['button']} onClick={remove}>
-					<LocalMallRoundedIcon style={{color: 'white'}}/>
+				<button className={styles['remove']} onClick={remove}>
+					<ClearIcon sx={{color: '#FE724C'}}/>
 				</button>
 			</div>
 		</div>
--- src/store/cart.slice.ts
+++ src/store/cart.slice.ts
@@ -17,8 +17,33 @@ export const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
 	reducers: {
+		delete: (state, action: PayloadAction<number>) => {
+			state.items = state.items.filter(i => i.id !== action.payload);
+		},
+
+		remove: (state, action: PayloadAction<number>) => {
+			const existed = state.items.find(i => i.id === action.payload);
+			
+			if(!existed){
+				return;
+			}
+
+			if(existed.count === 1) {
+			    state.items = state.items.filter(i => i.id !== action.payload);
+			}else{
+				state.items.map(i => {
+					if(i.id === action.payload){
+						i.count -= 1;
+					}
+
+					return i;
+				});
+				return; 
+			}
+		},
+        
 		add: (state, action: PayloadAction<number>) => {
-			const existed = state.items.find(i => i.id == action.payload);
+			const existed = state.items.find(i => i.id === action.payload);
 			
 			if(!existed){
 				state.items.push({ id: action.payload, count: 1});
