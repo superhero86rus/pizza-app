6232555eeb461440b2954519de146fc0794c6157
Kotov Aleksandr <kotov.alexandr@gmail.com>
2025-01-26 14:58:14 +0300

    Хранение JWT в RTK Storage

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -41,8 +41,6 @@ export function Login(){
 				password
 			});
 
-			console.log(data);
-			localStorage.setItem('jwt', data.access_token);
 			dispatch(userActions.addJwt(data.access_token));
 			navigate('/');
 		}catch(e){
